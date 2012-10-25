a5e691143b51b68f1768fcd3c8e337d89c42e695
chaudhary <amit.chaudhary@navyuginfo.com>
2012-10-25 10:58:55 +0530

    created a test branch

---
+++ myapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '25f60fd13cfeb101c931144c385f93af828d5e70f7639cd214f541bd4dac8987cf8d33b951fb65be4564d53a402b023e01546b96177ffe84153bce5bf6447ddd'
